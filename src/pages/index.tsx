adfc9f041494d8b6f8cbe1840286c4ef7ab7916a
gustavosilvaf <gustavofsilva@email.com>
2021-07-20 10:19:16 -0300

    Create subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
 
@@ -19,6 +20,8 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 monthly</span>
           </p>
+
+          <SubscribeButton />
         </section>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/images/avatar.svg" alt="Girl coding" />
