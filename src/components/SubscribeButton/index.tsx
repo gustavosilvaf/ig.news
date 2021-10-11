370eab63f6a374aa6731e1bd19a83e1ae2f5da85
gustavosilvaf <gustavofsilva@email.com>
2021-10-11 13:30:48 -0300

    Create redirect

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import styles from "./styles.module.scss";
 import {signIn, useSession} from "next-auth/client";
+import {api} from "../../services/api";
+import {getStripeJs} from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -7,13 +9,21 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const [session] = useSession();
-    const handleSubscribe = () => {
+    const handleSubscribe = async () => {
         if(!session) {
             signIn('github')
             return;
         }
+        try {
+            const response = await api.post('/subscribe')
+            const { sessionId } = response.data;
 
+            const stripe = await getStripeJs()
 
+            await stripe.redirectToCheckout({sessionId})
+        } catch (err) {
+            console.error(err.message)
+        }
 
     }
 
