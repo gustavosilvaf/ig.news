4eafa3280f8470f2f4fe7a761261f28b0fa57c9e
gustavosilvaf <gustavofsilva@email.com>
2021-10-11 16:23:52 -0300

    Transform active link in dynamic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,24 @@
 import styles from "./styles.module.scss";
 import { SignInButton } from "../SignInButton";
 import Link from "next/link";
+import {useRouter} from "next/router";
+import {ActiveLink} from "../ActiveLink";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href='/'>
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href='/'>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
