b2698166e2e47fb6b463f64a37d85e29f7392598
gustavosilvaf <gustavofsilva@email.com>
2021-10-11 15:42:18 -0300

    Created post page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.scss';
+
+export default function Posts() {
+    return (
+        <>
+            <head>
+                <title>Posts | Ignews</title>
+            </head>
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Como renomear vários arquivos de uma vez usando o terminal</strong>
+                        <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
+                    </a>
+                </div>
+
+            </main>
+        </>
+    );
+}
